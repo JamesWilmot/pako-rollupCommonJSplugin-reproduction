24b51fdb4f93f5328bd35493f6b2bd81d03a77b7
James Wilmot <jameswilmot2000@gmail.com>
2024-02-08 17:07:42 +1100

    Put together basic reproduction

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "opendata.fit development environment for the webapp and frontend packages";
+  description = "basic frontend flake";
 
   # Use the unstable nixpkgs to use the latest set of node packages
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
